225a96292e699a0669e9667928e18d9cc524be26
Rayyan Aamir <92763211+rayyanaamir22@users.noreply.github.com>
2023-10-23 22:14:45 -0400

    refactor framework

---
+++ Math/linalg/Vector.java
@@ -0,0 +1,15 @@
+package Math.linalg;
+
+public class Vector {
+    /*
+     * number of columns in the vector
+     */
+    public int this.columns;
+
+    /*
+     * vector constructor
+     */
+    public vector(int columns) {
+        this.columns = columns;
+    }
+}
