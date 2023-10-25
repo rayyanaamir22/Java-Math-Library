dd10b054dde3442181953ed455644261b6d7f0c9
Rayyan Aamir <92763211+rayyanaamir22@users.noreply.github.com>
2023-10-24 22:33:17 -0400

    Started implementing Matrix

--- JMath/linalg/Matrix.java
+++ JMath/linalg/Matrix.java
@@ -1,5 +1,55 @@
 package JMath.linalg;
+import JMath.linalg.MatrixCreationException;
 
 public class Matrix {
-    
+    /*
+     * Number of rows in the Matrix.
+     */
+    public int rows;
+
+    /*
+     * Number of columns in the Matrix.
+     */
+    public int columns;
+
+    /*
+     * Storage of Matrix entries.
+     */
+    private double[][] grid;
+
+    /*
+     * Matrix constructor method.
+     */
+    public Matrix(double[][] arr) throws MatrixCreationException {
+        if (Matrix.isValidMatrix(arr)) {
+            this.grid = arr;
+            // TODO: initialize row, columns
+        } else {
+            throw MatrixCreationException("arr is not of the appropriate dimensions to construct a Matrix.");
+        }
+    }
+
+    /*
+     * Return true iff the 2D array can be compiled as a Matrix in the constructor.
+     */
+    public static boolean isValidMatrix(double[][] arr) {
+        // make sure all rows are the same length
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1].length != arr[i].length) {
+                return false;
+            }
+        } return true;
+    }
+
+    /*
+     * Print the Matrix.
+     */
+    public void printMatrix() {
+        for (int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[i].length; j++) {
+                System.out.print(this.grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ JMath/linalg/MatrixCreationException.java
@@ -0,0 +1,10 @@
+package JMath.linalg;
+
+/*
+ * For errors during construction of a Matrix object.
+ */
+public class MatrixCreationException extends Exception {
+    public MatrixCreationException(String message) {
+        super(message);
+    }
+}
